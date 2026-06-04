22c42ba00328b268d53c9bf118860863ea0bfb4e
aspolito <aspolito@gmail.com>
2026-06-04 16:11:01 -0700

    Fix: silent throw/bow ammunition was creating sonic booms
    Silent ammo needs to stay silent. If it is set to be silent, keep it silent.

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -1180,10 +1180,14 @@ static int calc_gun_volume( const item &gun )
     int noise = parent.type->gun->loudness;
     // Check the ammo data first so that subsonic ammo is suppressable by gun mods.
     if( gun.ammo_data() ) {
-        noise += gun.ammo_data()->ammo->loudness;
-        // Speed of sound at sea level is around 343 meters per second.
-        if( gun.ammo_data()->ammo->speed > 342 ) {
-            noise = std::max( 120, noise );
+        // if ammo has an explicit loudness of 0, keep it quiet even if it's supersonic do to legacy ammuition
+        // and all sorts of strangeness with speed settings.
+        if ( gun.ammo_data()->ammo->loudness > 0 ) {
+            noise += gun.ammo_data()->ammo->loudness;
+            // Speed of sound at sea level is around 343 meters per second.
+            if( gun.ammo_data()->ammo->speed > 342 ) {
+                noise = std::max( 120, noise );
+            }
         }
     }
     for( const auto mod : parent.gunmods() ) {
